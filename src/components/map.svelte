980ee9c48f452ae55110482922868c8885fbdcdd
TiagoAryan <tiago_aryan@hotmail.com>
2020-04-23 18:16:14 +0100

    show cases on map

--- src/components/map.svelte
+++ src/components/map.svelte
@@ -1,193 +1,149 @@
 <script>
   import { onMount } from "svelte";
 
-  export let timelineData;
   export let concelhoData;
-  export let type = true;
+  var map;
 
-  $: concelhoData, placeAllCircles();
+  let Municip;
+
+  $: map && concelhoData, placeAllCircles();
+  $: Municip && getMunicip();
 
   onMount(() => {
-    var bounds = [[36, -6], [42, -9]];
-    var selectedConselho;
-    var first_run=false;
+    let bounds = [[30, -6], [43, -31]];
+    let selectedConselho;
 
-    mapboxgl.setRTLTextPlugin('https://cdn.maptiler.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.1.2/mapbox-gl-rtl-text.js');
-    mapboxgl.accessToken='pk.eyJ1IjoiYmpkaW9nbyIsImEiOiJjazg3bW40dnkwbjYwM2htbWc1NnBidzQ2In0.lh4trQ8-6vDRegpJWs6mBw';
-    var map = new mapboxgl.Map({
-      container: 'map',
-      style: 'https://api.maptiler.com/maps/5ce0b2a2-d5dc-44ae-84f3-7211439b9474/style.json?key=TLbKST4hnYUY3nc3yvDh',
-      center: [-8, 38],
-      zoom: 6,
-      minZoom: 6,
+    mapboxgl.setRTLTextPlugin(
+      "https://cdn.maptiler.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.1.2/mapbox-gl-rtl-text.js"
+    );
+    mapboxgl.accessToken =
+      "pk.eyJ1IjoiYmpkaW9nbyIsImEiOiJjazg3bW40dnkwbjYwM2htbWc1NnBidzQ2In0.lh4trQ8-6vDRegpJWs6mBw";
+    map = new mapboxgl.Map({
+      container: "map",
+      style:
+        "https://api.maptiler.com/maps/5ce0b2a2-d5dc-44ae-84f3-7211439b9474/style.json?key=TLbKST4hnYUY3nc3yvDh",
+      center: [-18.732, 38.342],
+      zoom: 5,
+      minZoom: 1,
       maxZoom: 8
     });
-    
-  map.on('load', function() {
+
+    map.on("load", function() {
       // Add source for admin-1 Boundaries
-      map.addSource('conselhos', {
-              'type': 'geojson',
-              'data':  './mapp.geojson',
-              'generateId': true 
-          });
-      
-
-
- 
-        map.addLayer({
-            'id': 'conselhos-fills',
-            'type': 'fill',
-            'source': 'conselhos',
-            'layout': {},
-            'paint': {
-                'fill-color': '#627BC1',
-                'fill-opacity': [
-                    'case',
-                    ['boolean', ['feature-state', 'hover'], false],
-                    1,
-                    0.5
-                ]
-            }
-        });
-         map.on('click', 'conselhos-fills', function(e) {
-            if (e.features.length > 0) {
-                //var features = map.queryRenderedFeatures({ layers: ['conselhos-fills'] });
-                //var uniqueFeatures = getUniqueFeatures(features, 'cartodb_id');
-                if (selectedConselho) {
-                    map.setFeatureState(
-                        { source: 'conselhos',  id: selectedConselho },
-                        { hover: false }
-                    );
-                }
-                selectedConselho = e.features[0].id;
-                map.setFeatureState(
-                    { source: 'conselhos', id: selectedConselho },
-                    { hover: true }
-                );
-            }
-        });
-
-        function getUniqueFeatures(array, comparatorProperty) {
-          var existingFeatureKeys = {};
-          // Because features come from tiled vector data, feature geometries may be split
-          // or duplicated across tile boundaries and, as a result, features may appear
-          // multiple times in query results.
-          var uniqueFeatures = array.filter(function(el) {
-          if (existingFeatureKeys[el.properties[comparatorProperty]]) {
-          return false;
-          } else {
-          existingFeatureKeys[el.properties[comparatorProperty]] = true;
-          return true;
-          }
-          });
-          
-        return uniqueFeatures;
-        }
-
+      map.addSource("conselhos", {
+        type: "geojson",
+        data: "./map.geojson",
+        generateId: true
       });
 
-  map.on('idle',function(){
-    console.log("idle");
-    //your code here 
-    if(!first_run){
-    console.log("ONE TIME");
-
-      var features = map.queryRenderedFeatures({ layers: ['conselhos-fills'] });
-      var num_circles = 10;
-       var points_coord = {
-              type: "FeatureCollection",
-              features: []
-        };
-
-      for (var r = 0; r < features.length; r++) {
-        var bound = L.latLngBounds(L.geoJson(features[r]).getBounds());
-        var center_country = bound.getCenter();
-        var x_max = bound.getEast();
-        var x_min = bound.getWest();
-        var y_max = bound.getSouth();
-        var y_min = bound.getNorth();
-        var j = 0;
-        
-        while (j < num_circles) {
-          //random positions
-          var lat = y_min + Math.random() * (y_max - y_min);
-          var lng = x_min + Math.random() * (x_max - x_min);
-          var point_pos = L.latLng(lat, lng);
-          //circle visuals
-          var circleOptions = {
-            color: "#ff0000",
-            fillColor: "#ff0000",
-            fillOpacity: 0.3,
-            weight: 1,
-            interactive: false
-          };
-          console.log(features[r].geometry);
-          
-          if ( features[r].geometry.type == "MultiPolygon") {
-            for ( var m = 0;  m <  features[r].geometry.coordinates.length;m++ ) {
-              if (inside(lat, lng, features[r].geometry.coordinates[m][0])) {
-                
-                // Coordinates are initially set to origin.
-                var point = {
-                      type: "Feature",
-                      properties: {},
-                      geometry: {
-                        type: "Point",
-                        coordinates: [lng, lat]
-                      }
-                    }
-                  
-                
-                points_coord.features.push(point);
-                j++;
-              }
-            }
-
-          }else{
-             if (inside(lat, lng, features[r].geometry.coordinates[0])) {
-              
-              // Coordinates are initially set to origin.
-              var point = {
-                    type: "Feature",
-                    properties: {},
-                    geometry: {
-                      type: "Point",
-                      coordinates: [lng, lat]
-                    }
-                  }
-                
-              
-              points_coord.features.push(point);
-              j++;
-            }
-          }
+      map.addLayer({
+        id: "conselhos-fills",
+        type: "fill",
+        source: "conselhos",
+        layout: {},
+        paint: {
+          "fill-color": "#627BC1",
+          "fill-opacity": [
+            "case",
+            ["boolean", ["feature-state", "hover"], false],
+            1,
+            0.5
+          ]
         }
-      }
-      
-       map.addSource('source-points', {
-          type: "geojson",
-          data: points_coord
+      });
+      map.on("mousemove", "conselhos-fills", function(e) {
+        map.getCanvas().style.cursor = "pointer";
+        if (e.features.length > 0) {
+          if (selectedConselho) {
+            map.setFeatureState(
+              { source: "conselhos", id: selectedConselho },
+              { hover: false }
+            );
+          }
+          selectedConselho = e.features[0].id;
+          map.setFeatureState(
+            { source: "conselhos", id: selectedConselho },
+            { hover: true }
+          );
+        }
+      });
+
+      map.on("mouseleave", "conselhos-fills", function() {
+        if (selectedConselho) {
+          map.setFeatureState(
+            { source: "conselhos", id: selectedConselho },
+            { hover: false }
+          );
+        }
+        selectedConselho = null;
+
+        popup.remove();
+      });
+      var popup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: false
+      });
+      map.on("click", "conselhos-fills", e => {
+        Municip = e.features[0].properties.Municipality;
+
+        var bound = L.latLngBounds(L.geoJson(e.features[0]).getBounds());
+        var center_country = bound.getCenter();
+
+        let cc = 0;
+        if (
+          concelhoData.filter(
+            c =>
+              e.features[0].properties.Municipality.toUpperCase() ===
+              c.attributes.Concelho
+          )[0]
+        )
+          cc = concelhoData.filter(
+            c =>
+              e.features[0].properties.Municipality.toUpperCase() ===
+              c.attributes.Concelho
+          )[0].attributes.ConfirmadosAcumulado_Conc;
+
+        var coordinates = center_country;
+        var description =
+          "<strong>" +
+          e.features[0].properties.Municipality +
+          "</strong><p>" +
+          cc +
+          " Cases</p>";
+
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        // Populate the popup and set its coordinates
+        // based on the feature found.
+        popup
+          .setLngLat(coordinates)
+          .setHTML(description)
+          .addTo(map);
+      });
+
+      function getUniqueFeatures(array, comparatorProperty) {
+        var existingFeatureKeys = {};
+        // Because features come from tiled vector data, feature geometries may be split
+        // or duplicated across tile boundaries and, as a result, features may appear
+        // multiple times in query results.
+        var uniqueFeatures = array.filter(function(el) {
+          if (existingFeatureKeys[el.properties[comparatorProperty]]) {
+            return false;
+          } else {
+            existingFeatureKeys[el.properties[comparatorProperty]] = true;
+            return true;
+          }
         });
 
-        map.addLayer({
-              'id': 'infected',
-              'type': 'circle',
-              'source': 'source-points',
-              'paint': {
-              // make circles larger as the user zooms from z12 to z22
-              'circle-radius': {
-                'base': 1.75,
-                'stops': [[12, 2], [22, 180]]
-                },
-              // color circles by ethnicity, using a match expression
-              'circle-color': '#fbb03b'
-              }
-        });
-      first_run=true;
-    }
-    })
-  
- });
+        return uniqueFeatures;
+      }
+    });
+  });
 
   function inside(y, x, vs) {
     // ray-casting algorithm based on
@@ -207,13 +163,118 @@
     return inside;
   }
   function placeAllCircles() {
-    if (!timelineData && !concelhoData) return;
-    if (type) {
-      let data = concelhoData.filter(e => "COIMBRA" == e.attributes.Concelho);
-      console.info(data);
-    } else {
-      console.info(timelineData);
-    }
+    if (!concelhoData) return;
+    let first_run = false;
+    map.on("idle", function() {
+      if (!first_run) {
+        var features = map.queryRenderedFeatures({
+          layers: ["conselhos-fills"]
+        });
+
+        var points_coord = {
+          type: "FeatureCollection",
+          features: []
+        };
+
+        for (var r = 0; r < features.length; r++) {
+          if (
+            concelhoData.filter(
+              e =>
+                features[r].properties.Municipality.toUpperCase() ===
+                e.attributes.Concelho
+            )[0]
+          ) {
+            var bound = L.latLngBounds(L.geoJson(features[r]).getBounds());
+            var center_country = bound.getCenter();
+            var x_max = bound.getEast();
+            var x_min = bound.getWest();
+            var y_max = bound.getSouth();
+            var y_min = bound.getNorth();
+            var j = 0;
+
+            var num_circles =
+              concelhoData.filter(
+                e =>
+                  features[r].properties.Municipality.toUpperCase() ===
+                  e.attributes.Concelho
+              )[0].attributes.ConfirmadosAcumulado_Conc / 10;
+
+            while (j < num_circles) {
+              //random positions
+              var lat = y_min + Math.random() * (y_max - y_min);
+              var lng = x_min + Math.random() * (x_max - x_min);
+              var point_pos = L.latLng(lat, lng);
+              // console.log(features[r].geometry);
+
+              if (features[r].geometry.type == "MultiPolygon") {
+                for (
+                  var m = 0;
+                  m < features[r].geometry.coordinates.length;
+                  m++
+                ) {
+                  if (
+                    inside(lat, lng, features[r].geometry.coordinates[m][0])
+                  ) {
+                    // Coordinates are initially set to origin.
+                    var point = {
+                      type: "Feature",
+                      properties: {},
+                      geometry: {
+                        type: "Point",
+                        coordinates: [lng, lat]
+                      }
+                    };
+
+                    points_coord.features.push(point);
+                    j++;
+                  }
+                }
+              } else {
+                if (inside(lat, lng, features[r].geometry.coordinates[0])) {
+                  // Coordinates are initially set to origin.
+                  var point = {
+                    type: "Feature",
+                    properties: {},
+                    geometry: {
+                      type: "Point",
+                      coordinates: [lng, lat]
+                    }
+                  };
+
+                  points_coord.features.push(point);
+                  j++;
+                }
+              }
+            }
+          }
+        }
+
+        map.addSource("source-points", {
+          type: "geojson",
+          data: points_coord
+        });
+
+        map.addLayer({
+          id: "infected",
+          type: "circle",
+          source: "source-points",
+          paint: {
+            // make circles larger as the user zooms from z12 to z22
+            "circle-radius": {
+              base: 1.75,
+              stops: [[12, 2], [22, 180]]
+            },
+            // color circles by ethnicity, using a match expression
+            "circle-color": "#fbb03b"
+          }
+        });
+        first_run = true;
+      }
+    });
+  }
+
+  function getMunicip() {
+    console.info(Municip);
   }
 
   function changeType() {
